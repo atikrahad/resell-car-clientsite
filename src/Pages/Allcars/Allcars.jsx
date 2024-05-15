284c2527be1435d02e27e5cf62d4f643f52caa2e
Atik Rahad <atikrahad1@gmail.com>
2024-05-15 10:36:02 +0600

    adding search functionality in all cars

--- src/Pages/Allcars/Allcars.jsx
+++ src/Pages/Allcars/Allcars.jsx
@@ -6,10 +6,30 @@ import Publicaxios from "../../Api/Publicaxios";
 import Cars from "./Allcarscomponents/Cars";
 
 const Allcars = () => {
-    const [carsdata, setCarsdata] = useState([]);
-    useEffect(() => {
-      Publicaxios.get("/resentcars").then((res) => setCarsdata(res.data));
-    }, []);
+  const [carsdata, setCarsdata] = useState([]);
+  const [brandName, setBrandName] = useState();
+  const [price, setPrice] = useState();
+  console.log(brandName)
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setBrandName(e.target.brand.value);
+    setPrice(e.target.price.value);
+    e.target.reset()
+  };
+
+  useEffect(() => {
+    if(brandName || price){
+      Publicaxios.get(`/allcars?name=${brandName}&price=${price}`).then((res) =>
+        setCarsdata(res.data)
+      );
+    }else{
+      Publicaxios.get(`/allcars?`).then((res) =>
+        setCarsdata(res.data)
+      );
+
+    }
+  }, [brandName, price]);
   return (
     <div className="">
       <Navber
@@ -23,8 +43,9 @@ const Allcars = () => {
         img={img}
         search={
           <div className="p-8  rounded-3xl bg-white">
-            <form>
-              <select name="" className="bg-transparent" id="">
+            <form onSubmit={handleSearch} className="flex items-center justify-between">
+              <select name="brand" className="bg-transparent outline-none w-[40%]" id="">
+                <option value={''}>Select Brand Name</option>
                 <option value="audi">Audi</option>
                 <option value="bmw">BMW</option>
                 <option value="marcedes">Marcedes</option>
@@ -32,7 +53,8 @@ const Allcars = () => {
                 <option value="nissan">Nissan</option>
                 <option value="nissan">Nissan</option>
               </select>
-              <input type="submit" value={"Search car"} />
+              <input type="number" placeholder="Price" className="outline-none bg-white w-[40%]" name="price" />
+              <input type="submit" className="md:text-2xl" value={"Search car"} />
             </form>
           </div>
         }
